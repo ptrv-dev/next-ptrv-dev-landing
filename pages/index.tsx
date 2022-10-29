96d1d2d8b7b59ac10813edd461028b304d16d6e5
ptrv-dev <petrov99119@gmail.com>
2022-10-29 23:06:24 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-extrabold">Index Page</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
